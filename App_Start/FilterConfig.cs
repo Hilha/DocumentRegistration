94c364ee06e353e926efa5318eea3688eb21ddaf
hilha <energystar1998>
2021-02-23 14:01:00 -0300

    Adicionar arquivos de projeto.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentRegistration
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
